a3477cf1724d5e97373e5a0e4ca0ac3b48c6e1c6
cxcarvaj <cxcarvaj@gmail.com>
2025-05-09 06:58:33 -0500

    Initial Commit

---
+++ SecretApp/SecretAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretAppApp.swift
+//  SecretApp
+//
+//  Created by Carlos Xavier Carvajal Villegas on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SecretAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
